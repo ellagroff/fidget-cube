54e17d34d3ce0ca1336337e61d5f35d8d3b24f58
ellagroff <122560331+ellagroff@users.noreply.github.com>
2023-01-20 12:58:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showString("Can I help?")
+})
 input.onButtonPressed(Button.A, function () {
     basic.showString("yes")
 })
